ea75914a54090e63538e0104bf678f5b207a654b
Francisco Pereira <157219261+Paaquiitoo22@users.noreply.github.com>
2026-03-04 20:27:18 +0100

    Interfaz bienvenida (#3)
    * Propuesta inicial interfaz de bienvenida con prueba de estilo css y orden en la documentacion
    
    * Propuesta inicial interfaz de bienvenida con prueba de estilo css y orden en la documentacion
    
    * Propuesta inicial interfaz de bienvenida con prueba de estilo css y orden en la documentacion
    
    * Propuesta inicial interfaz de bienvenida con prueba de estilo css y orden en la documentacion
    
    * [REF] Documentación: borrado los documentos repetidos.
    
    ---------
    
    Co-authored-by: diegosangodoy2002 <diegosangodoy2002@gmail.com>

---
+++ src/main/java/com/tfg/ajedrez/AjedrezApplication.java
@@ -0,0 +1,20 @@
+package com.tfg.ajedrez;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AjedrezApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader=new FXMLLoader(AjedrezApplication.class.getResource("/com/tfg/ajedrez/vista/bienvenida.fxml"));
+        Scene escena=new Scene(loader.load(),800,800);
+        stage.setScene(escena);
+        stage.setTitle("¡Bienvenido!");
+        stage.show();
+
+    }
+}
---
+++ src/main/java/com/tfg/ajedrez/controller/BienvenidaController.java
@@ -0,0 +1,6 @@
+package com.tfg.ajedrez.controller;
+
+public class BienvenidaController {
+
+
+}
