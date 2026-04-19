4ecae6129330c475594763936eb6027365eb05f6
diegosangodoy2002 <diegosangodoy2002@gmail.com>
2026-04-19 23:04:18 +0200

    Corregir interacción del menú y capas en menú principal fxml
    - Corregida la lógica para cerrar el menú lateral al pulsar fuera
    - Arreglado el botón de nueva partida que no detectaba clics por solapamiento de capas

--- src/main/java/com/tfg/ajedrez/controller/MenuPrincipalController.java
+++ src/main/java/com/tfg/ajedrez/controller/MenuPrincipalController.java
@@ -4,28 +4,53 @@ import com.tfg.ajedrez.AjedrezApplication;
 import com.tfg.ajedrez.util.SceneManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+
+import java.sql.Struct;
 
 public class MenuPrincipalController {
 
-
     @FXML
     VBox menuDesplegable;
+
+    @FXML
+    Region RegionMenu;
 
     @FXML
     public void onVolver(ActionEvent event) throws Exception {
         SceneManager.navegarA("/com/tfg/ajedrez/vista/login.fxml");
     }
 
+    /**
+     * Método que cierra y abre el menú desplegable cuando pulsas en el boton del
+     * menú
+     *
+     */
     @FXML
-    public void onDesplegable(ActionEvent event)throws Exception{
+    public void onDesplegable(ActionEvent event) throws Exception {
 
-       if (menuDesplegable.isVisible()){
-           menuDesplegable.setVisible(false);
-       }
-        else{
+        if (menuDesplegable.isVisible()) {
+            menuDesplegable.setVisible(false);
+            RegionMenu.setVisible(false);
+        } else {
             menuDesplegable.setVisible(true);
-       }
+            RegionMenu.setVisible(true);
+        }
+    }
+
+    /**
+     * Método que cierra el menú desplegable cuando se pulsa en cualquier
+     * parte de la pantalla del menú principal
+     */
+
+    @FXML
+    public void onCerrarMenu() {
+
+        if (menuDesplegable.isVisible() && RegionMenu.isVisible()) {
+            menuDesplegable.setVisible(false);
+            RegionMenu.setVisible(false);
+        }
     }
 
     /**
@@ -34,7 +59,7 @@ public class MenuPrincipalController {
      */
 
     @FXML
-    public void onNuevaPartida(){
+    public void onNuevaPartida() {
         SceneManager.navegarA("/com/tfg/ajedrez/vista/nueva-partida.fxml");
     }
 }
