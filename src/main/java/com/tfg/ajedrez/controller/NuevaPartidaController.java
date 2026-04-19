e45373a070fa043334a3d98547d337fe98dbbbc2
diegosangodoy2002 <diegosangodoy2002@gmail.com>
2026-04-19 23:22:51 +0200

    Plantilla de la ventana “nueva partida” creada

--- src/main/java/com/tfg/ajedrez/controller/NuevaPartidaController.java
+++ src/main/java/com/tfg/ajedrez/controller/NuevaPartidaController.java
@@ -1,4 +1,12 @@
 package com.tfg.ajedrez.controller;
 
+import com.tfg.ajedrez.util.SceneManager;
+import javafx.fxml.FXML;
+
 public class NuevaPartidaController {
+
+    @FXML
+    public void onVolver(){
+        SceneManager.navegarA("/com/tfg/ajedrez/vista/menu-principal.fxml");
+    }
 }
