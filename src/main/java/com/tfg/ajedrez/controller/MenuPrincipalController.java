cc39ea5bed7e90c9465952b46996dfdce301fc5d
diegosangodoy2002 <diegosangodoy2002@gmail.com>
2026-04-19 15:46:16 +0200

    Cambios interfaz menu principal:
    - Implementada fuente Inter.
    - Añadido panel de estadísticas y menú desplegable en la pantalla principal.
    - Limpieza de recursos (fuentes antiguas) e inclusión de nuevos iconos en el menu desplegable.

--- src/main/java/com/tfg/ajedrez/controller/MenuPrincipalController.java
+++ src/main/java/com/tfg/ajedrez/controller/MenuPrincipalController.java
@@ -1,13 +1,40 @@
 package com.tfg.ajedrez.controller;
 
+import com.tfg.ajedrez.AjedrezApplication;
 import com.tfg.ajedrez.util.SceneManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class MenuPrincipalController {
+
+
+    @FXML
+    VBox menuDesplegable;
 
     @FXML
     public void onVolver(ActionEvent event) throws Exception {
         SceneManager.navegarA("/com/tfg/ajedrez/vista/login.fxml");
+    }
+
+    @FXML
+    public void onDesplegable(ActionEvent event)throws Exception{
+
+       if (menuDesplegable.isVisible()){
+           menuDesplegable.setVisible(false);
+       }
+        else{
+            menuDesplegable.setVisible(true);
+       }
+    }
+
+    /**
+     * Método que hace que al pulsar el botón de Nueva partida te lleve
+     * a la ventana de nueva partida
+     */
+
+    @FXML
+    public void onNuevaPartida(){
+        SceneManager.navegarA("/com/tfg/ajedrez/vista/nueva-partida.fxml");
     }
 }
