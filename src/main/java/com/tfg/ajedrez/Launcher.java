2a9f1afd2fba1f488a2e31ad802b0bee9af4124e
ivan <ivan@example.com>
2026-05-06 13:37:37 +0200

    para tener el .exe

---
+++ src/main/java/com/tfg/ajedrez/Launcher.java
@@ -0,0 +1,9 @@
+package com.tfg.ajedrez;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(AjedrezApplication.class, args);
+    }
+}
